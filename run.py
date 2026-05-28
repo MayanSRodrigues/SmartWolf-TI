80a50690bdbd0c8515e241d01ea4067bf80adf79
MayanSRodrigues <mayansobral@hotmail.com>
2026-05-28 19:54:48 -0300

    adicionar init_db para criar tabelas

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app, db
-from app.models import Equipamento, Emprestimo
+from app.models import Equipamento, Emprestimo, Chamado, Usuario
 import sys
+import os
 
 app = create_app()
 
@@ -9,16 +10,16 @@ def make_shell_context():
     return {
         'db':          db,
         'Equipamento': Equipamento,
-        'Emprestimo':  Emprestimo
+        'Emprestimo':  Emprestimo,
+        'Chamado':     Chamado,
+        'Usuario':     Usuario
     }
 
 if __name__ == '__main__':
     modo = sys.argv[1] if len(sys.argv) > 1 else 'dev'
-
     if modo == 'prod':
-        print("🚀 Servidor rodando em modo PRODUÇÃO")
-        print("📡 Acesso da equipe: http://10.0.0.11:5000")
-        app.run(debug=False, host='0.0.0.0', port=5000)
+        print("🚀 SmartWolf TI — Modo PRODUÇÃO")
+        app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
     else:
-        print("🔧 Servidor rodando em modo DESENVOLVIMENTO")
+        print("🔧 SmartWolf TI — Modo DESENVOLVIMENTO")
         app.run(debug=True, host='0.0.0.0', port=5000)
